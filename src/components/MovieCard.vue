12226c54e6d4c9b622282f0bc5322ea3e5a98c18
jkarlin929 <osida11572@gmail.com>
2022-02-27 15:12:02 -0500

    edit css and change Home to MovieCard component and router updates

--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -27,16 +27,16 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.event-card {
-  padding: 20px;
-  width: 250px;
+.movie-card {
+  padding: 16px;
+  width: 350px;
   cursor: pointer;
-  border: 1px solid #39495c;
+  border: 1px solid #8e00009e;
   margin-bottom: 18px;
 }
 
-.event-card:hover {
+.movie-card:hover {
   transform: scale(1.01);
-  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+  box-shadow: 0 5px 12px 0 rgba(0, 0, 0, 0.2);
 }
 </style>
