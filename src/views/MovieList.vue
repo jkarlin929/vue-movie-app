ac7389ac609fd93b8516716c2318b8ed9210817c
jkarlin929 <osida11572@gmail.com>
2022-02-27 15:48:45 -0500

    get data from axios call

--- src/views/MovieList.vue
+++ src/views/MovieList.vue
@@ -7,11 +7,33 @@
 <script>
 // @ is an alias to /src
 import MovieCard from "@/components/MovieCard.vue";
+import axios from 'axios';
 
 export default {
-  name: "Home",
+  name: "MovieList",
   components: {
     MovieCard,
   },
+  data() {
+    return {
+      movies: null
+    }
+  },
+  created() {
+    axios.get('http://www.omdbapi.com/?i=tt3896198&apikey=9bbf7263&t=star')
+    .then(res => {
+      console.log(res)
+      this.movies  = res.data
+    })
+    .catch(e => {
+      console.log(e)
+    })
+  }
 };
 </script>
+
+<style scoped>
+.home {
+  display: inline-block;
+}
+</style>
